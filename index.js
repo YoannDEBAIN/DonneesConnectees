f681d7fabbab457e0a12c8de191a2927e2bf6d95
Yoann DEBAIN <debainyoann@gmail.com>
2023-03-13 18:10:57 +0100

    post

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ var app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("", function(req, res){
-	res.send("salut toto!");
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// var cors = require('cors');
+
+var data=[];
+
+app.use(express.static('html'));
+
+// app.post("/annotation", cors(), function(req, res){
+	
+app.post("/annotation", function(req, res){
+	var body = req.body;
+	// console.log(body);
+	data.push(body);
+	// console.log(data);
+	res.send("votre commentaire a été sauvegardé !");
 });
+
+// app.get("/IdURI", function(req, res){
+	// res.send("salut toto!");
+// });
 
 app.listen(port, function(){
 	console.log('serveur listening on port : '+port);
