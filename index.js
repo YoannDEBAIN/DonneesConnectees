eeb51801e3eb25c7b25a7cc8f2f73931ec5744a5
Yoann DEBAIN <debainyoann@gmail.com>
2023-03-13 14:39:15 +0100

    gestion port

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.get("/toto", function(req, res){
+	res.send("salut toto!");
+});
 
 app.listen(port, function(){
 	console.log('serveur listening on port : '+port);
