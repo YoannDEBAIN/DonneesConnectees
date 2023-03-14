37ee48cd53fba4717cff5c67f9b818832c26c207
Yoann DEBAIN <debainyoann@gmail.com>
2023-03-14 10:13:46 +0100

    ajout get

--- index.js
+++ index.js
@@ -6,25 +6,131 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// var cors = require('cors');
-
-var data=[];
+var data={};
+var id=0;
 
 app.use(express.static('html'));
 
-// app.post("/annotation", cors(), function(req, res){
 	
 app.post("/annotation", function(req, res){
 	var body = req.body;
 	// console.log(body);
-	data.push(body);
-	// console.log(data);
-	res.send("votre commentaire a été sauvegardé !");
+	data[id]=body;
+	// data.push(body);
+	console.log(data);
+	id++;
+	res.send();
 });
 
-// app.get("/IdURI", function(req, res){
-	// res.send("salut toto!");
-// });
+
+app.get("/IdAnnot", function(req, res){
+	var IdAnnot = req.query.Annot;
+	
+	var Exist=Object.keys(data).includes(IdAnnot);
+	
+	var ChoixFormat=req.query.FormatIdAnnot;
+	
+	console.log(req.headers['accept']);
+	
+	if (ChoixFormat=="html"){
+		req.headers['accept']= 'text/html';
+	}
+	else {
+		if (ChoixFormat=="Json"){
+			req.headers['accept']=  'application/json';
+		}	
+	}
+	
+	console.log(req.headers['accept']);
+	
+	res.format ({
+		   'text/html': function() {
+			    if (Exist){
+				   res.send(data[IdAnnot]); 
+			    }
+			    else {
+				   res.send("aucune annotation n'est associée à cette clé");
+			    }
+		   },
+
+		   'application/json': function() {
+			    if (Exist){
+				   res.send(data[IdAnnot]); 
+			    }
+			    else {
+				   res.send("aucune annotation n'est associée à cette clé");
+			    }
+			}
+	});
+	
+});
+
+
+
+
+app.get("/AllAnnot", function(req, res){
+	var ChoixFormat=req.query.FormatAllAnnot;
+	
+	
+	if (ChoixFormat=="html"){
+		req.headers['accept']= 'text/html';
+	}
+	else {
+		if (ChoixFormat=="Json"){
+			req.headers['accept']=  'application/json';
+		}	
+	}
+		
+	res.format ({
+		   'text/html': function() {
+			  res.send(data); 
+		   },
+
+		   'application/json': function() {
+			  res.send(data);
+			}
+	});
+	
+});
+
+
+app.get("/URI", function(req, res){
+	var IdURI = req.query.AnnotURI;
+	
+	var ChoixFormat=req.query.FormatAnnotURI;
+	
+	var tabRep=[]
+	
+	for (key in data){
+		if (data[key]["URI"]==IdURI){
+			tabRep.push({"IdAnnotation" : data[key], "Commentaire" : data[key]["Commentaire"]});
+		}
+	}
+	
+	if (ChoixFormat=="html"){
+		req.headers['accept']= 'text/html';
+	}
+	else {
+		if (ChoixFormat=="Json"){
+			req.headers['accept']=  'application/json';
+		}	
+	}
+		
+	res.format ({
+		   'text/html': function() {
+			  res.send(tabRep); 
+		   },
+
+		   'application/json': function() {
+			  res.send(tabRep);
+			}
+	});
+	
+});
+
+
+
+
 
 app.listen(port, function(){
 	console.log('serveur listening on port : '+port);
